feeb96e7d828b17eb210d1e119a4b314ca874b0d
dtbuild <dtbuild@rigil>
2026-08-19 10:55:14 +0000

    45b01aab249f3db4e4ccc5e5a8972c3dd2ee659e Fix: Typescript options for DataTables options (main and columns) were incomplete (requires DataTables 3.0.3 for the columns).
    Sync to source repo @45b01aab249f3db4e4ccc5e5a8972c3dd2ee659e

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,5 +1,4 @@
-/// <reference types="jquery" />
-import DataTables, { Api as Api$1 } from 'datatables.net';
+import DataTables, { Api } from 'datatables.net';
 export { default } from 'datatables.net';
 
 declare const icons: {
@@ -159,7 +158,7 @@ declare class Button {
      * Create a new button for use in ColumnControl contents. Buttons created by this class can be
      * used at the top level in the header or in a dropdown.
      */
-    constructor(dt: Api$1, host: ColumnControl);
+    constructor(dt: Api, host: ColumnControl);
     /**
      * Check if anything is making this button active
      *
@@ -224,7 +223,7 @@ declare class CheckList {
      * @param dt DataTable instance
      * @param idx Column index
      */
-    searchListener(dt: Api$1): this;
+    searchListener(dt: Api): this;
     /**
      * Select all buttons
      *
@@ -259,7 +258,7 @@ declare class CheckList {
     /**
      * Container for a list of buttons
      */
-    constructor(dt: Api$1, host: ColumnControl, opts: IOptions);
+    constructor(dt: Api, host: ColumnControl, opts: IOptions);
     /**
      * Update the deselect counter
      */
@@ -820,7 +819,7 @@ declare class SearchInput {
     /**
      * Create a container element, for consistent DOM structure and styling
      */
-    constructor(dt: Api$1, idx: number, columnUnique: number);
+    constructor(dt: Api, idx: number, columnUnique: number);
     /**
      * Load a DataTables state
      *
@@ -866,7 +865,7 @@ declare class ColumnControl {
      *
      * @returns DataTables API instance
      */
-    dt(): Api$1<any>;
+    dt(): Api<any>;
     /**
      * Get what column index this instance of ColumnControl is operating on
      *
@@ -902,7 +901,7 @@ declare class ColumnControl {
      * @param columnIdx Column index to operation on
      * @param opts Configuration options
      */
-    constructor(dt: Api$1, columnIdx: number, opts: IConfig);
+    constructor(dt: Api, columnIdx: number, opts: IConfig);
     /**
      * Resolve the configured target into a DOM element
      */
@@ -956,224 +955,172 @@ declare class ColumnControl {
     static version: string;
 }
 
-// Type definitions for DataTables ColumnControl
-//
-// Project: https://datatables.net/extensions/columncontrol/, https://datatables.net
-// Definitions by:
-//   SpryMedia
-//   Andy Ma <https://github.com/andy-maca>
-
-
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * DataTables' types integration
- */
 declare module 'datatables.net' {
-	interface Config {
-		/**
-		 * Common ColumnControl extension options to apply to all columns
-		 */
-		columnControl?: ConfigColumnControl | Array<TContentItem | ConfigColumnControl>;
-	}
-
-	interface ConfigColumns {
-		/**
-		 * Column specific column configuration options
-		 *
-		 * @returns Api for chaining with the additional ColumnControl methods
-		 */
-		columnControl?: ConfigColumnControl | Array<TContentItem | ConfigColumnControl>;
-	}
-
-	interface DataTablesStatic {
-		/**
-		 * ColumnControl class
-		 */
-		ColumnControl: {
-			/**
-			 * Create a new ColumnControl instance for a specific column
-			 */
-			new (
-				dt: Api<any>,
-				columnIdx: number,
-				config: ConfigColumnControl
-			): DataTablesStatic['ColumnControl'];
-
-			/**
-			 * ColumnControl version
-			 */
-			version: string;
-
-			/**
-			 * Default configuration values
-			 */
-			defaults: ConfigColumnControl;
-		};
-	}
-
-	interface ConfigLanguage {
-		/** Column Control language options */
-		columnControl: {
-			/** Column visibility title */
-			colVis?: string;
-
-			/** Column visibility button text */
-			colVisDropdown?: string;
-
-			/** Dropdown button text */
-			dropdown?: string;
-
-			/** Add to ordering (ascending) button text */
-			orderAddAsc?: string;
-
-			/** Add to ordering (descending) button text */
-			orderAddDesc?: string;
-
-			/** Set ordering (ascending) button text */
-			orderAsc?: string;
-
-			/** Clear all ordering button text */
-			orderClear?: string;
-
-			/** Set ordering (descending) button text */
-			orderDesc?: string;
-
-			/** Remove column from multi-ordering button text */
-			orderRemove?: string;
-
-			/** Column reorder button text */
-			reorder?: string;
-
-			/** Move column left button text */
-			reorderLeft?: string;
-
-			/** Move column right button text */
-			reorderRight?: string;
-
-			/** Clear search button text */
-			searchClear?: string;
-
-			/** Search dropdown button text */
-			searchDropdown?: string;
-
-			/** Search list dropdown button text */
-			searchList?: string;
-
-			/** Spacer text */
-			spacer?: string;
-
-			/** List strings, used for searchList and colVis */
-			list?: {
-				/** Select all link text */
-				add?: string;
-
-				/** Label for when there is no label */
-				empty?: string;
-
-				/** Select none link text */
-				none?: string;
-
-				/** Search placeholder */
-				search?: string;
-			};
-
-			/** Strings used for the <select> available for choosing different search logic */
-			search?: {
-				/** searchText options */
-				text?: {
-					equal?: string;
-					notEqual?: string;
-					starts?: string;
-					ends?: string;
-					empty?: string;
-					notEmpty?: string;
-				};
-
-				/** searchDateTime options */
-				datetime?: {
-					equal?: string;
-					notEqual?: string;
-					greater?: string;
-					less?: string;
-					empty?: string;
-					notEmpty?: string;
-				};
-
-				/** searchNumber options */
-				number?: {
-					equal?: string;
-					notEqual?: string;
-					greater?: string;
-					greaterOrEqual?: string;
-					less?: string;
-					lessOrEqual?: string;
-					empty?: string;
-					notEmpty?: string;
-				};
-			};
-		};
-	}
-
-	interface ApiColumnMethods<T> {
-		/** Methods for ColumnControl */
-		columnControl: {
-			/**
-			 * Clear any ColumnControl search that is applied to the selected column (both
-			 * `searchList` and the input search types will be cleared).
-			 */
-			searchClear(): ApiColumnMethods<T>;
-
-			/**
-			 * Reload the options for the `searchList` content type, or provide new options.
-			 *
-			 * @param options Options to load in, or use `'refresh'` to read from the table.
-			 */
-			searchList(
-				options: 'refresh' | string[] | Array<{ label: string; value: any }>
-			): ApiColumnMethods<T>;
-		};
-	}
-
-	interface ApiColumnsMethods<T> {
-		columnControl: {
-			/**
-			 * Clear any ColumnControl search that is applied to the selected columns (both
-			 * `searchList` and the input search types will be cleared).
-			 */
-			searchClear(): ApiColumnMethods<T>;
-
-			/**
-			 * Reload the options for the `searchList` content type, or provide new options.
-			 *
-			 * @param options Options to load in, or use `'refresh'` to read from the table.
-			 */
-			searchList(
-				options: 'refresh' | string[] | Array<{ label: string; value: any }>
-			): ApiColumnMethods<T>;
-		};
-	}
+    interface Options {
+        /**
+         * Common ColumnControl extension options to apply to all columns
+         */
+        columnControl?: ConfigColumnControl | Array<TContentItem | ConfigColumnControl>;
+    }
+    interface ColumnOptions {
+        /**
+         * Column specific column configuration options
+         *
+         * @returns Api for chaining with the additional ColumnControl methods
+         */
+        columnControl?: ConfigColumnControl | Array<TContentItem | ConfigColumnControl>;
+    }
+    interface DataTablesStatic {
+        /**
+         * ColumnControl class
+         */
+        ColumnControl: {
+            /**
+             * Create a new ColumnControl instance for a specific column
+             */
+            new (dt: Api<any>, columnIdx: number, config: ConfigColumnControl): DataTablesStatic['ColumnControl'];
+            /**
+             * ColumnControl version
+             */
+            version: string;
+            /**
+             * Default configuration values
+             */
+            defaults: ConfigColumnControl;
+        };
+    }
+    interface Language {
+        /** Column Control language options */
+        columnControl: {
+            /** Column visibility title */
+            colVis?: string;
+            /** Column visibility button text */
+            colVisDropdown?: string;
+            /** Dropdown button text */
+            dropdown?: string;
+            /** Add to ordering (ascending) button text */
+            orderAddAsc?: string;
+            /** Add to ordering (descending) button text */
+            orderAddDesc?: string;
+            /** Set ordering (ascending) button text */
+            orderAsc?: string;
+            /** Clear all ordering button text */
+            orderClear?: string;
+            /** Set ordering (descending) button text */
+            orderDesc?: string;
+            /** Remove column from multi-ordering button text */
+            orderRemove?: string;
+            /** Column reorder button text */
+            reorder?: string;
+            /** Move column left button text */
+            reorderLeft?: string;
+            /** Move column right button text */
+            reorderRight?: string;
+            /** Clear search button text */
+            searchClear?: string;
+            /** Search dropdown button text */
+            searchDropdown?: string;
+            /** Search list dropdown button text */
+            searchList?: string;
+            /** Spacer text */
+            spacer?: string;
+            /** List strings, used for searchList and colVis */
+            list?: {
+                /** Select all link text */
+                add?: string;
+                /** Label for when there is no label */
+                empty?: string;
+                /** Select none link text */
+                none?: string;
+                /** Search placeholder */
+                search?: string;
+            };
+            /** Strings used for the <select> available for choosing different search logic */
+            search?: {
+                /** searchText options */
+                text?: {
+                    equal?: string;
+                    notEqual?: string;
+                    starts?: string;
+                    ends?: string;
+                    empty?: string;
+                    notEmpty?: string;
+                };
+                /** searchDateTime options */
+                datetime?: {
+                    equal?: string;
+                    notEqual?: string;
+                    greater?: string;
+                    less?: string;
+                    empty?: string;
+                    notEmpty?: string;
+                };
+                /** searchNumber options */
+                number?: {
+                    equal?: string;
+                    notEqual?: string;
+                    greater?: string;
+                    greaterOrEqual?: string;
+                    less?: string;
+                    lessOrEqual?: string;
+                    empty?: string;
+                    notEmpty?: string;
+                };
+            };
+        };
+    }
+    interface ApiColumnMethods<T> {
+        /** Methods for ColumnControl */
+        columnControl: {
+            /**
+             * Clear any ColumnControl search that is applied to the selected column (both
+             * `searchList` and the input search types will be cleared).
+             */
+            searchClear(): ApiColumnMethods<T>;
+            /**
+             * Reload the options for the `searchList` content type, or provide new options.
+             *
+             * @param options Options to load in, or use `'refresh'` to read from the table.
+             */
+            searchList(options: 'refresh' | string[] | Array<{
+                label: string;
+                value: any;
+            }>): ApiColumnMethods<T>;
+        };
+    }
+    interface ApiColumnsMethods<T> {
+        columnControl: {
+            /**
+             * Clear any ColumnControl search that is applied to the selected columns (both
+             * `searchList` and the input search types will be cleared).
+             */
+            searchClear(): ApiColumnMethods<T>;
+            /**
+             * Reload the options for the `searchList` content type, or provide new options.
+             *
+             * @param options Options to load in, or use `'refresh'` to read from the table.
+             */
+            searchList(options: 'refresh' | string[] | Array<{
+                label: string;
+                value: any;
+            }>): ApiColumnMethods<T>;
+        };
+    }
 }
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Options
- */
 type TTfootTarget = `tfoot:${number}`;
 type TTheadTarget = `thead:${number}`;
-
 interface ConfigColumnControl {
-	/**
-	/**
-	 * Designate the target header or footer row for where to insert the ColumnControl's content
-	 */
-	target: number | 'tfoot' | TTfootTarget | TTheadTarget;
-
-	/**
-	 * Class(es) to assign to the target row.
-	 */
-	className?: string | string[];
-
-	/**
-	 * Content to show in the cells.
-	 */
-	content?: TContentItem[];
+    /**
+    /**
+     * Designate the target header or footer row for where to insert the ColumnControl's content
+     */
+    target: number | 'tfoot' | TTfootTarget | TTheadTarget;
+    /**
+     * Class(es) to assign to the target row.
+     */
+    className?: string | string[];
+    /**
+     * Content to show in the cells.
+     */
+    content?: TContentItem[];
 }
