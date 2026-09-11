4fdb8071118daaf24fdabdbd5f10d40864b9b24a
dtbuild <dtbuild@rigil>
2026-09-11 11:50:14 +0000

    208b33e63326bc11ce24dd28ff84ea717846e27b Release 2.0.2
    Sync to source repo @208b33e63326bc11ce24dd28ff84ea717846e27b

--- js/dataTables.columnControl.js
+++ js/dataTables.columnControl.js
@@ -1,4 +1,4 @@
-/*! ColumnControl 2.0.1 for DataTables
+/*! ColumnControl 2.0.2 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  *
  * SVG icons: ISC License
@@ -2968,7 +2968,7 @@ ColumnControl.defaults = {
 /** SVG icons that can be used by the content plugins */
 ColumnControl.icons = icons;
 /** Version */
-ColumnControl.version = '2.0.1';
+ColumnControl.version = '2.0.2';
 
 
 if (!DataTable || !DataTable.versionCheck || !DataTable.versionCheck('3')) {
